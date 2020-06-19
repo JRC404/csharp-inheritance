cc30617add098c4156846a99c3a90a2646d4ca78
Jacob <jacob.reilly.cooper@gmail.com>
2020-06-19 17:12:20 +0100

    inheritance added

---
+++ Inheritance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car jacobCar = new Car();
+            Motorbike jacobBike = new Motorbike();
+            jacobCar.beep();
+            jacobBike.beep();
+            jacobBike.wheelie();
+            // jacobCar.wheelie(); // whaaaa?
+            
+
+            Console.WriteLine(jacobCar.brand);
+        }
+    }
+}
---
+++ Inheritance/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class Vehicle
+    {
+        public string brand = "Mercedes";
+        public void beep()
+        {
+            Console.WriteLine("Beep.");
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public string modelName = "A Class";
+    }
+
+    class Motorbike : Vehicle
+    {
+        public string modelName = "Harley";
+
+        public void wheelie()
+        {
+            Console.WriteLine("What a rush.");
+        }
+    }
+}
